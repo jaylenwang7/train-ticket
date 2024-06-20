45ee5a94e613190149809a390210351197e3382e
jaylenwang7 <jaylencwang@gmail.com>
2024-06-20 11:35:14 -0400

    fix repo

---
+++ setup-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Install maven
+apt-get update
+apt-get install -y maven
+
+# Install java 8
+apt-get install -y openjdk-8-jdk
+
+# Check docker is installed
+if ! [ -x "$(command -v docker)" ]; then
+  echo 'Error: docker is not installed.' >&2
+  exit 1
+fi
+
+# Switch java to version 8
+update-alternatives --set java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java
+
+# Clean up
+mvn clean package -Dmaven.test.skip=true
