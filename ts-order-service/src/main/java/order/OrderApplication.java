b998f51d0655706a86a7e90d6be31ce04c36d813
jaylenwang7 <jaylencwang@gmail.com>
2025-03-21 13:05:54 -0400

    try this

--- ts-order-service/src/main/java/order/OrderApplication.java
+++ ts-order-service/src/main/java/order/OrderApplication.java
@@ -2,14 +2,14 @@ package order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+// import org.springframework.boot.web.client.RestTemplateBuilder;
 
 
-import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.web.client.RestTemplate;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -23,10 +23,5 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
-    }
-
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
     }
 }
